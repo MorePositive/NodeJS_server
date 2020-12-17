a7cff499d3cc6318c056b27d270cd7ac13fd45d3
MorePositive <morepositivecode@gmail.com>
2020-12-17 23:35:47 +0200

    fixed post, put, delete methods

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
     const URL = "mongodb+srv://MorePositive:bEsrT8O1m2aUJW5M@cluster0.y2laz.mongodb.net/vehiclesDB?retryWrites=true&w=majority";
     mongoose.connect(URL, { 
         useNewUrlParser: true,
-        useFindAndModify: true,
+        useFindAndModify: false,
         useUnifiedTopology: true 
     })
     .then(() => {
@@ -32,7 +32,7 @@ app.get("/contacts", () => console.log("You are on 'Contacts' page"));
 app.get("/folder", () => createFolder());
 app.get("/newfile", () => createFile());
 
-app.get("/add-car", () => {
+app.post("/add-car", () => {
     const car = new Car({
         mark: "Mercedes",
         model: "C43AMG",
@@ -45,19 +45,33 @@ app.get("/add-car", () => {
 
 app.get("/cars", () => {
     Car.find()
+        .then(data => {
+            if (!data.length) {
+                console.log("You have not stored data");
+            } else {
+                console.log(data);
+            }
+        })
+        .catch(error => console.log(error));
+});
+
+app.get("/car/:id", (req) => {
+    const id = req.params.id;
+    Car.findById(id)
         .then(data => console.log(data))
         .catch(error => console.log(error));
 });
 
-app.get("/car", () => {
-    Car.findById("5fd9d6eb6702912f3c607b39")
-        .then(data => console.log(data))
+app.put("/update/:id", (req) => {
+    const id = req.params.id;
+    Car.findOneAndUpdate({ _id: id }, { mark: "BMW" }, { new: true })
+        .then((data) => console.log(data))
         .catch(error => console.log(error));
 });
 
-app.get("/delete/:id", (req, res) => {
+app.delete("/delete/:id", (req) => {
   const id = req.params.id;
   Car.findOneAndDelete(id)
     .then(() => console.log(`car with id: ${id} has been deleted`))
-    .catch((error) => console.log(error))
+    .catch(error => console.log(error));
 });
